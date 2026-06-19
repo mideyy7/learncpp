a4e9534df09f9363563f873a4a0f625ea3c1157a
Ayomide <mideyy7@gmail.com>
2026-06-19 01:31:12 +0100

    a

---
+++ chapter_16/vector.cpp
@@ -0,0 +1,23 @@
+
+/*
+
+
+NOTES:
+
+Length is the number of elements currently in use.
+Capacity is the number of elements that have storage allocated.
+
+1. .size() and .capacity() of vectors
+2. .resize(). ... shrinking and enlarging changes length.....only enlarging changes capacity i.e capacity does not change when shrinking
+3. if you want to force capacity to change with shrink use .shrink_to_fit()
+
+
+
+
+
+
+
+
+
+
+*/
--- chapter_5/solve.cpp
+++ chapter_5/solve.cpp
@@ -20,4 +20,10 @@ int main() {
 
 
     return 0;
+
+
+
+    //  A program called a profiler can be used to see how long various parts of the program are taking to run, and which are impacting overall performance
+
+    
 }
