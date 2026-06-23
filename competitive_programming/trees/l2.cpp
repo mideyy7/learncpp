48221b3de13f7ec0be2c0b4b0a5e2338e6479bc9
Ayomide <mideyy7@gmail.com>
2026-06-23 22:52:04 +0100

    implement

--- competitive_programming/trees/l2.cpp
+++ competitive_programming/trees/l2.cpp
@@ -1,20 +1,23 @@
 #include <vector>
 
 
-
-struct TreeNode {
-    int value;
-    TreeNode* left;
-    TreeNode* right;
-    explicit TreeNode(int value) : TreeNode(value, nullptr, nullptr) {}
-    TreeNode(int value, TreeNode* left, TreeNode* right): value(value), left(left), right(right) {}
-};
+#include "tree.h"
+#include "traversal.h"
 
 int main() {
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(3);
     // include <memory> to use auto new_node = std::make_unique<TreeNode>(4);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->left->right->left = new TreeNode(6);
+    root->right->left = new TreeNode(7);
+    root->right->right = new TreeNode(8);
+    root->right->right->left = new TreeNode(9);
+    root->right->right->right = new TreeNode(10);
+
+    in_order(root);
     return 0;
 }
 
---
+++ competitive_programming/trees/traversal.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "tree.h"
+
+void pre_order(TreeNode* node);
+void post_order(TreeNode* node);
+void in_order(TreeNode* node);
+void level_order(TreeNode* node);
