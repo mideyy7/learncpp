48941dd4f2fa9bf6815e6cfed6e19fb5c69feb0c
Ayomide <mideyy7@gmail.com>
2026-06-23 22:35:25 +0100

    change file names

--- competitive_programming/trees/l4.cpp
+++ competitive_programming/trees/l4.cpp
@@ -1,20 +1,48 @@
-/*
-Traversals
-DFS
-1. In order Traversal
-- left , root, right
-- root should be IN
 
-2. Pre Order Traversal
-- root, left, right
-- root should be PRE
+#include <iostream>
+#include <queue>
 
-3. Post Order Traversal
-- left , right, root
-- root should be POST
 
-BFS
-1. Level Order Traversal
-- go through each level one after the other
+#include "tree.h"
 
-*/
+// note best practice is to use if (!node) instead of if (node == nullptr)
+
+void pre_order(TreeNode* node) {
+    if (node == nullptr) return;
+    std::cout << node->value << ' '; 
+    pre_order(node->left);
+    pre_order(node->right);
+}
+
+void in_order(TreeNode* node) {
+    if (node == nullptr) return;
+    in_order(node->left);
+    std::cout << node->value << ' ';
+    in_order(node->right);
+}
+
+void post_order(TreeNode* node) {
+    if (node == nullptr) return;
+    post_order(node->left);
+    post_order(node->right);
+    std::cout << node->value << ' ';
+}
+
+void level_order(TreeNode* node) {
+    if (node == nullptr) return;
+    std::queue<TreeNode* > q;
+    q.push(node);
+    while (!q.empty()) {
+        int dim = q.size();
+        for (int i = 0; i < dim; i++) {
+            TreeNode* node = q.front();
+            q.pop();
+            std::cout << node->value << ' ';
+            if (node->left != nullptr) q.push(node->left);
+            if (node->right != nullptr) q.push(node->right);
+        }
+
+    }
+
+
+}
