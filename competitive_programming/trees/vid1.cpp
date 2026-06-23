0b9350f3544b17a73d7fc0f953a968cc4b941c52
Ayomide <mideyy7@gmail.com>
2026-06-23 21:18:45 +0100

    trees

---
+++ competitive_programming/trees/vid1.cpp
@@ -0,0 +1,19 @@
+
+
+
+/*
+
+Trees
+1. root node
+2. leaf node
+3. child
+4. ancestor
+
+Types:
+1. Complete: every level is completely filled (last might not be filled mut all leaf nodes must be leftward)
+2. Perfect: All leaf nodes are on the same level
+3. Degenerate: Skewed as each node has just 1 child
+4. Full: All nodes has 0 ir 2 children
+5. Balanced: heighht of the tree is log n where n is no. of nodes
+
+*/
