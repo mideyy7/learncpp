3286de64493030513f95cbdb4a76b818b3ad0609
Ayomide <mideyy7@gmail.com>
2026-06-29 01:30:46 +0100

    top

--- competitive_programming/trees/l8.cpp
+++ competitive_programming/trees/l8.cpp
@@ -39,3 +39,58 @@ std::vector<std::vector<int>> zig_zag(TreeNode* node, std::vector<std::vector<in
     }
     return result;
 }
+
+/*
+Boundary traversal
+->  ANTICLOCKWISE: left boundary then leaf nodes then right boundary in reverse
+-> CLOCKWISE: right boundary then leaf nodes then left boundary in reverse
+-> use pre order trsversal to get the leaf nodes
+
+*/
+bool is_leaf(TreeNode* node) {
+    return (!node->left && !node->right);
+}
+void add_left_boundary(TreeNode* root, std::vector<int>& res) {
+    TreeNode* node = root->left;
+    while (node) {
+        if (!is_leaf(node)) res.push_back(node->value);
+        if (node->left)  {
+            node = node->left;
+        } else {
+            node = node->right;
+        }
+     }
+}
+
+void add_leaves(TreeNode* node, std::vector<int>& res) {
+    if (!node) return;
+    if (is_leaf(node)) res.push_back(node->value);
+    add_leaves(node->left, res);
+    add_leaves(node->right, res);
+}
+
+void add_right_boundary(TreeNode* root, std::vector<int>& res) {
+    TreeNode* node = root->right;
+    std::vector<int> temp;
+    while (node) {
+        temp.push_back(node->value);
+        if (node->right) {
+            node = node->right;
+        } else {
+            node = node->left;
+        }
+    }
+    for (int i = temp.size() - 1; i >= 0; i--) {
+        res.push_back(temp[i]);
+    }
+}
+
+std::vector<int> anti_clockwise_boundary(TreeNode* root) {
+    std::vector<int> result;
+    if (!root) return result;
+    if (!is_leaf(root)) result.push_back(root->value);
+    add_left_boundary(root, result);
+    add_leaves(root, result);
+    add_right_boundary(root, result);
+    return result;
+}
---
+++ competitive_programming/trees/l9.cpp
@@ -0,0 +1,54 @@
+
+
+#include <vector>
+#include <map>
+#include <utility>
+#include <queue>
+
+#include "tree.h"
+
+
+/*
+
+perform a level order traversal going level by level
+for left -> +1,,,,,,, for right -> -1
+add to the queue.... (node, position)
+pop from the queue and check if it exists in map
+if not exit add to map: if it exusts skip
+iterate through map and pick values
+
+*/
+
+std::vector<int> top_view(TreeNode* root) {
+    std::vector<int> result;
+    if (!root) return result;
+
+    std::map<int, int> mp;
+    std::queue<std::pair<TreeNode*, int>> q;
+    q.push({root, 0});
+    while (!q.empty()) {
+        size_t dim = q.size();
+        for (size_t i = 0; i < dim; i++) {
+             auto [node, position] = q.front(); q.pop();
+            if (mp.find(position) == mp.end()) {
+                mp[position] = node->value;
+            }
+            if (node->left) q.push({node->left,position - 1});
+            if (node->right) q.push({node->right, position + 1});
+        }
+    }
+
+    for (const auto& [position, value]: mp) {
+        result.push_back(value);
+    }
+    return result;
+}
+
+/*
+Bottom View
+
+Go through the tree by level order traversal
+in the queue.. push {node, position}
+if left child exists: posiion - 1, if right child exists 
+
+*/
