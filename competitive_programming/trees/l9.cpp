e4bd8a136a8a0e41affea3eab488899024119a22
Ayomide <mideyy7@gmail.com>
2026-06-29 01:37:57 +0100

    bottom

--- competitive_programming/trees/l9.cpp
+++ competitive_programming/trees/l9.cpp
@@ -49,6 +49,30 @@ Bottom View
 
 Go through the tree by level order traversal
 in the queue.. push {node, position}
-if left child exists: posiion - 1, if right child exists 
-
+if left child exists: posiion - 1, if right child exists position +1
+after pop from queue update the position in map
 */
+
+std::vector<int> bootom_view(TreeNode* root) {
+    std::vector<int> result;
+    if (!root) return result;
+
+    std::map<int, int>mp;
+    std::queue<std::pair<TreeNode*,int>>q;
+    q.push({root,0});
+
+    while (!q.empty()) {
+        auto [node, position] = q.front(); q.pop();
+        mp[position] = node->value;
+        if (node->left) q.push({node->left, position - 1});
+        if (node->right) q.push({node->right, position + 1});
+    }
+
+    for (const auto& [position, value] : mp){
+        result.push_back(value);
+    }
+    return result;
+
+
+
+}
