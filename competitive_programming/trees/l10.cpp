32b62ed9dbcf883a26d6285b42cba1778ce4db72
Ayomide <mideyy7@gmail.com>
2026-07-07 18:27:36 +0100

    fresh

--- competitive_programming/trees/l10.cpp
+++ competitive_programming/trees/l10.cpp
@@ -14,12 +14,34 @@ on each pop from queue add to result
 
 std::vector<int> right_view(TreeNode* root) {
     std::vector<int> result;
-    if (!root) return result;
     std::queue<TreeNode*> q;
-    q.push(root);
+    if (root) q.push(root);
     while (!q.empty()) {
-        size_t dim = 
-    }
-
-
+        size_t dim = q.size();
+        for (size_t i = 0; i < dim; i++) {
+            TreeNode* node = q.front(); q.pop();
+            if (i == 0) result.push_back(node->value);
+            if (node->right) q.push(node->right);
+            if (node->left) q.push(node->left);
+        }
+     }
+     return result;
 }
+
+
+std::vector<int> left_view(TreeNode* root) {
+    std::vector<int> result;
+    std::queue<TreeNode*> q;
+    if (root) q.push(root);
+    while (!q.empty()) {
+        size_t dim = q.size();
+        for (size_t i = 0; i < dim; i++) {
+            TreeNode* node = q.front(); q.pop();
+            if (i == 0) result.push_back(node->value);
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+    }
+    return result;
+}
+
